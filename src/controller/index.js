931d1febb359ce046484a012b565518fba5abcb4
Victor Comette <victor.ribeiro@cwi.com.br>
2020-08-13 21:04:27 -0300

    clean files

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,7 +2,6 @@ import express from 'express'
 
 import { authController } from './auth'
 import { loginController } from './login'
-import { townhouseController } from './townhouse'
 import { homeController } from './home'
 import { subscriberController } from './subscriber'
 import { productController } from './product'
@@ -16,7 +15,6 @@ export function configRoutes(app) {
   app.use(baseRoute)
   app.use(authController)
   app.use(loginController)
-  app.use(townhouseController)
   app.use(homeController)
   app.use(subscriberController)
   app.use(productController)
